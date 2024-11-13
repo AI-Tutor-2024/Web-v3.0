a6023d66d6a672433de68daaabf10cbb2ac5319c
sangmaaaaan <matthew10164@gmail.com>
2024-11-13 19:04:12 +0900

    fix: fix api to apiClient

--- src/app/api/folders/index.ts
+++ src/app/api/folders/index.ts
@@ -1,65 +1,93 @@
 // folderApi.ts
-import { FolderListData, FolderListResponse, PatchFolderProps, PostFolderProps } from "@/app/types/folder";
+import {
+  FolderListData,
+  FolderListResponse,
+  PatchFolderProps,
+  PostFolderProps,
+} from "@/app/types/folder";
 import { baseURL } from "..";
 import axios from "axios";
+import apiClient from "@/app/utils/api";
 
 // 폴더 목록 가져오기
 export const getFolders = async (): Promise<FolderListData[]> => {
-  const response = await axios.get(`${baseURL}/api/v1/folder`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-  return response.data.information; // `information` 배열만 반환
-};
-
-// 폴더 생성
-export const createFolder = async ({ folderName, professorName }: PostFolderProps) => {
-const response = await axios.post(`${baseURL}/api/v1/folder`, {
-  folderName,
-  professorName,
-}, {
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-return response.data;
-};
-
-// 폴더 수정
-export const updateFolder = async ({ folderId, folderName, professorName }: PatchFolderProps) => {
-const response = await axios.patch(`${baseURL}/api/v1/folder/${folderId}`, {
-  folderName,
-  professorName,
-}, {
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-return response.data;
-};
-
-// 폴더 삭제
-export const deleteFolder = async (folderId: number) => {
-const response = await axios.delete(`${baseURL}/api/v1/folder/${folderId}`, {
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-return response.data;
-};
-
-// 폴더 이름 가져오기 (test)
-export const fetchFolderName = async () => {
-  const response = await axios.get(`${baseURL}/api/v1/folder/names`, {
+  const response = await apiClient.get(`api/v1/folder/`, {
     headers: {
       "Content-Type": "application/json",
     },
   });
-  
+
+  if (response.status !== 200) {
+    console.log(response);
+    throw new Error("Failed to fetch folders");
+  }
+
+  const data: FolderListResponse = response.data;
+  return data.information; // `information` 배열만 반환
+};
+
+// 폴더 생성
+export const createFolder = async ({
+  folderName,
+  professorName,
+}: PostFolderProps) => {
+  const response = await apiClient.post(
+    `api/v1/folder/`,
+    {
+      folderName,
+      professorName,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (response.status !== 201) {
+    throw new Error("Failed to create folder");
+  }
+
   return response.data;
-  };
+};
+
+// 폴더 수정
+export const updateFolder = async ({
+  folderId,
+  folderName,
+  professorName,
+}: PatchFolderProps) => {
+  const response = await apiClient.patch(
+    `/api/v1/folder/${folderId}`,
+    {
+      folderName,
+      professorName,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (response.status !== 200) {
+    throw new Error("Failed to update folder");
+  }
+
+  return response.data;
+};
+
+// 폴더 삭제
+export const deleteFolder = async (folderId: number) => {
+  const response = await apiClient.delete(`/api/v1/folder/${folderId}`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.status !== 200) {
+    throw new Error("Failed to delete folder");
+  }
+
+  return response.data;
+};
